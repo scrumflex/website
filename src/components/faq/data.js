01f0403e1b6416ba7718e87f043d2c5d13388b2c
Gospel Amanze <ollygos@gmail.com>
2024-09-13 13:07:27 +0100

    UPDATE ON FAQ

--- src/components/faq/data.js
+++ src/components/faq/data.js
@@ -1,33 +1,33 @@
 const data = [
     {
         question: "What is Scrum training, and why do I need it?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        answer: "Scrum training is a learning experience that helps you understand and apply Scrum principles. It's beneficial if you work in project management, product development, or any collaborative environment. It makes work more organized and teams more efficient."
     },
     {
         question: " Who is Scrum training for?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        answer: "Scrum training is for anyone involved in projects, products or services– from team members to managers. It's especially useful for project managers, product managers, and those in the tech space who want to improve teamwork and project outcomes."
     },
     {
         question: "What will I learn in Scrum training?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." 
+        answer: "Scrum training covers the basics of Scrum, including roles, ceremonies, and artifacts. You'll learn how to apply Scrum to make projects smoother and improve collaboration and effectiveness in your team." 
     },
     {
         question: " How does Scrum training benefit my career?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        answer: "Scrum training makes you more valuable at work. It opens up opportunities for career growth, especially in the tech industry and project management. Employers appreciate professionals who understand and can apply agile methodologies like Scrum."
     },
     {
         question: "Do I need previous experience to take Scrum training?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." 
+        answer: "No, you don't need previous experience. Scrum training is designed for beginners, and it's a great way to start understanding how to make projects more efficient." 
         
     },
     {
         question: " Can Scrum training be applied to non-tech projects?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." 
+        answer: "Yes, absolutely! Scrum is versatile and can be applied to any project, not just tech-related ones. Whether you're planning events, managing teams, or developing products, Scrum principles can make the process more straightforward." 
         
     },
     {
         question: " What are the benefits of becoming a Scrum Master?",
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." 
+        answer: "As a Scrum Master, you become a Servant leader in your team. You help teams communicate better, coach the team to work effectively, and finish tasks on time. It's a rewarding role that contributes to project success and team satisfaction." 
         
     },
 ]
